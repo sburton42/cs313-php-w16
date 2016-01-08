00247294df121f8e4ce5c484cba393928361a21b
Scott Burton <scotthburton@gmail.com>
2016-01-07 17:41:29 -0700

    In class demos from 1/7

---
+++ inClassDemos/demo.js
@@ -0,0 +1,20 @@
+function changeColor() {
+
+
+	//$("#page-header-id").toggle();
+	$("#page-header-id").fadeOut();
+
+	// var color = document.getElementById("page-header-id").style.color;
+
+	// if (color == "blue") {
+	// 	color = "red";
+	// 	//document.getElementById("page-header-id").style.color = "red";
+	// } else {
+	// 	color = "blue";
+	// 	//document.getElementById("page-header-id").style.color = "blue";
+	// }
+
+	// document.getElementById("page-header-id").style.color = color;
+
+
+}
