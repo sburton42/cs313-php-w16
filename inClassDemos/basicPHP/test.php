a7495c43b0e3bc18e6035a87ff5f951dea6f7a0c
Scott Burton <scotthburton@gmail.com>
2016-01-14 10:39:02 -0700

    Adding broken code...on purpose

---
+++ inClassDemos/basicPHP/test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test HTML page</title>
+</head>
+
+<body>
+	<h1>Welcome to my page.</h1>
+<?php
+for ($i = 0; i < 10; $i++)
+{
+	lskdjflks
+	echo "This is from php" . ($i + 1) . "<br>\n";
+
+}
+?>
+</body>
+
+</html>
